ed126e0e884fd6fd439abb5330ba5f43d03a9bd9
DenisBelobrotski <belobrotskiy@gmail.com>
2020-12-19 15:15:13 +0300

    Code refactored

--- atmel_final_project/main.asm
+++ atmel_final_project/main.asm
@@ -1,3 +1,5 @@
+.def temp_0 = r16
+
 .org 0
 jmp reset
 .org 0x1C
@@ -5,39 +7,39 @@ jmp nextData
 
 reset:
 
-ldi r16,high(RAMEND)
-out SPH,r16
-ldi r16,low(RAMEND)
-out SPL,r16
+ldi temp_0, high(RAMEND)
+out SPH, temp_0
+ldi temp_0, low(RAMEND)
+out SPL, temp_0
 
-ldi r16,0
-out UBRRH,r16
-ldi r16,51
-out UBRRL,r16
+ldi temp_0, 0
+out UBRRH, temp_0
+ldi temp_0, 51
+out UBRRL, temp_0
 
-ldi r16,(1<<UDRIE)|(1<<TXEN)//|(1<<RXEN)
-out UCSRB,r16
+ldi temp_0, (1<<UDRIE)|(1<<TXEN)
+out UCSRB, temp_0
 
-ldi ZH,high(none_txt*2)
-ldi ZL,low(none_txt*2)
-lpm r16,Z+
-out UDR,r16
+ldi ZH, high(none_txt*2)
+ldi ZL, low(none_txt*2)
+lpm temp_0, Z+
+out UDR, temp_0
 
 sei
 
 end: rjmp end
 
 nextData:
-lpm r16,Z+
-out UDR,r16
+lpm temp_0, Z+
+out UDR, temp_0
 
-cpi ZH,high(end_none_txt*2)
+cpi ZH, high(end_none_txt*2)
 brne endData
-cpi ZL,low(end_none_txt*2)
+cpi ZL, low(end_none_txt*2)
 brne endData
 
-ldi ZH,high(none_txt*2)
-ldi ZL,low(none_txt*2)
+ldi ZH, high(none_txt*2)
+ldi ZL, low(none_txt*2)
 
 endData: reti
 
