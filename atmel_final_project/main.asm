90e45ad1098dc1d07128553049f1e4b541c5a977
DenisBelobrotski <belobrotskiy@gmail.com>
2020-12-30 12:30:45 +0300

    Unused code removed

--- atmel_final_project/main.asm
+++ atmel_final_project/main.asm
@@ -22,11 +22,6 @@ out UBRRL, temp_0
 ldi temp_0, (1<<UDRIE)|(1<<TXEN)
 out UCSRB, temp_0
 
-/*ldi ZH, high(none_txt * 2)
-ldi ZL, low(none_txt * 2)
-lpm temp_0, Z+
-out UDR, temp_0*/
-
 cbi DDRC, 5
 sbi PORTC, 5
 
@@ -46,23 +41,6 @@ end: rjmp end
 
 
 next_data:
-
-
-
-
-// check none end
-/*cpi switcher, 0b00000000
-brne skip_none_end_check
-
-cpi ZH, high(end_none_txt * 2)
-brne skip_none_end_check
-cpi ZL, low(end_none_txt * 2)
-brne skip_none_end_check
-
-ldi mutex, 0
-
-skip_none_end_check:*/
-
 
 // check btn_5 end
 sbrs switcher, 5
@@ -150,10 +128,6 @@ rjmp skip_7_click
 	or switcher, temp_0
 skip_7_click:
 
-/*ldi switcher, 0b11100000
-in temp_0, PINC
-and switcher, temp_0*/
-
 skip_buttons_check:
 
 
@@ -201,19 +175,8 @@ rjmp end_data
 
 skip_button_7_check:
 
-
-// no buttons clicked
-/*ldi ZH, high(none_txt * 2)
-ldi ZL, low(none_txt * 2)
-ldi mutex, 1
-rjmp end_data*/
-
 end_data: 
 reti
-
-none_txt:
-.db "-none-"
-end_none_txt:
 
 btn_5_txt:
 .db "-btn5-"
